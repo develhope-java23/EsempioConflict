e156ecc3d9a8283d9b03ae1a68176766df098c57
Fabrizio Lagioia <meandi958@gmail.com>
2024-04-10 15:45:27 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(buildFullName("Fabrizio", "Lagioia"));
+    }
+    public static String buildFullName (String name, String surname) {
+        return (name + " " + surname);
+    }
+}
